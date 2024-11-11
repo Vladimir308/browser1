846c227a97a04f04c7719c843243913215bdab04
Vladimir Milyukov <vova.milyukov.94@bk.ru>
2024-11-11 08:52:53 +0300

    init

--- src/test/java/tests/android/WikipediaTests.java
+++ src/test/java/tests/android/WikipediaTests.java
@@ -3,6 +3,8 @@ package tests.android;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import screens.android.WikipediaSearchResultsScreen;
+import screens.android.WikipediaSearchScreen;
 import tests.TestBase;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,24 +14,24 @@ public class WikipediaTests extends TestBase {
 
     @Test
     @DisplayName("Verify Successful Search Results in Wikipedia App")
-    public void  successfulSearchTest() {
+    public void successfulSearchTest() {
         String query = "Appium";
-        searchScreen.searchFor(query);
-        assertThat(searchResultsScreen.getResultsCount()).isGreaterThan(0);
+        WikipediaSearchScreen.searchFor(query);
+        assertThat(WikipediaSearchResultsScreen.getResultsCount()).isGreaterThan(0);
     }
 
     @Test
     @DisplayName("Verify Error Message for Unsuccessful Search in Wikipedia App")
     public void unsuccessfulSearchTest() {
         String query = "Java";
-        searchScreen.searchForUnsuccessful(query);
-        searchResultsScreen.ResultError();
+        WikipediaSearchScreen.searchForUnsuccessful(query);
+        WikipediaSearchResultsScreen.ResultError();
     }
 
     @Test
     @DisplayName("Checking the presence of the Log in button")
     public void checkLogInButtonTest() {
-        searchScreen.loginButton();
-        searchResultsScreen.ResultLogin();
+        WikipediaSearchScreen.loginButton();
+        WikipediaSearchResultsScreen.ResultLogin();
     }
 }
